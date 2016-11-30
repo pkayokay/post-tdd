73cee8142d92a310f1adcd86c38952cbb904099d
Paul Kim <pkayokay@gmail.com>
2016-11-30 19:10:19 +0000

    add title to post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,11 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
     
   def index
+    @posts = Post.order(created_at: :desc)
   end
 
   def new
+    @post = Post.new
   end
 
   def show
