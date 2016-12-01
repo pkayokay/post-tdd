edba3655601b1a7c28cb74127c95d44be4444a44
Paul Kim <pkayokay@gmail.com>
2016-12-01 02:53:34 +0000

    AWS bucket

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true
+  validates :picture, presence: true
   validates :caption, presence: true
   mount_uploader :picture, PictureUploader
 end
